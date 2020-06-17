daa2937305ad0d04d2a849f568b0ebc63a29f0bb
abdullanishad <abdullanishad00@gmail.com>
2020-06-17 21:32:12 +0530

    made changes for media serving

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -29,11 +29,18 @@ urlpatterns = [
 
 
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 #Add Django site authentication urls (for login, logout, password management)
 
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
