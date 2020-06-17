7374c52f6d9e68697a4e46bfd39a1bc0db246eab
abdullanishad <abdullanishad00@gmail.com>
2020-06-18 02:33:42 +0530

    ran collectstatic in local repo

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -147,9 +147,7 @@ STATIC_URL = '/static/'
 
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
